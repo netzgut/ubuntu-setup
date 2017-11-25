476e5c1187b88fb3464b9759a62ba26a2a0a208a
Benjamin Weidig <ben@netzgut.net>
2017-11-25 11:28:56 +0100

    added openvpn script

---
+++ scripts/openvpn.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+NAME="Open VPN"
+MARKER="openvpn"
+
+echo "Trying to install $NAME"
+
+if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
+    sudo apt-get install -y \
+        openvpn \
+        network-manager \
+        network-manager-openvpn \
+        network-manager-openvpn-gnome \
+    && date > $MARKER_DIRECTORY/$MARKER \
+    && echo "Finished installing $NAME"
+else
+    echo "$NAME is already installed"
+fi
+
