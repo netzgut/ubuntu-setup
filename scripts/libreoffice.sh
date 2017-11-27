c0f1779561ef9785c25429355915a5a2ed75c08b
Benjamin Weidig <ben@netzgut.net>
2017-11-27 08:19:11 +0100

    added libreoffice

---
+++ scripts/libreoffice.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+NAME="LibreOffice"
+MARKER="libreoffice"
+
+echo "Trying to install $NAME"
+
+if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
+    sudo apt-get purge -y libreoffice* \
+    && sudo add-apt-repository ppa:libreoffice/libreoffice-5-4 \
+    && sudo apt-get update \
+    && sudo apt-get install -y libreoffice  \
+    && date > $MARKER_DIRECTORY/$MARKER \
+    && echo "Finished installing $NAME"
+else
+    echo "$NAME is already installed"
+fi
+
