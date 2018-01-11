a10e663b75cac1762d50f8ff967c70c0fa2eaddf
Benjamin Weidig <ben@netzgut.net>
2018-01-11 18:44:19 +0100

    Fixed kernel 4.14.6

--- scripts/kernel-4.14.6.sh
+++ scripts/kernel-4.14.6.sh
@@ -5,7 +5,7 @@ set -e
 NAME="Kernel (4.14.6)"
 MARKER="kernel-4.14.6"
 
-BASE_URL="http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.1/"
+BASE_URL="http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.6/"
 HEADERS_ALL="linux-headers-4.14.6-041406_4.14.6-041406.201712140930_all.deb"
 HEADERS_AMD64="linux-headers-4.14.6-041406-generic_4.14.6-041406.201712140930_amd64.deb"
 IMAGE_AMD64="linux-image-4.14.6-041406-generic_4.14.6-041406.201712140930_amd64.deb"
