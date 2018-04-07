b3b58d6c30eff0a1e750a147cb85dfd0ed9e08d9
Benjamin Weidig <ben@netzgut.net>
2018-04-07 11:25:19 +0200

    jiraclient: ensure bin-folder exists

--- scripts/jiraclient.sh
+++ scripts/jiraclient.sh
@@ -10,6 +10,7 @@ echo "Trying to install $NAME"
 if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
     curl -L http://d1.almworks.com/.files/jiraclient-${JIRACLIENT_VERSION}_without_jre.tar.gz -o /tmp/jiraclient.tar.gz \
     && sudo tar xvfz /tmp/jiraclient.tar.gz -C /opt/ \
+    && mkdir -p $HOME/bin \
     && ln -s /opt/jiraclient-${JIRACLIENT_VERSION}/bin/jiraclient.sh $HOME/bin/jiraclient \
     && date > $MARKER_DIRECTORY/$MARKER \
     && echo "Finished installing $NAME"
