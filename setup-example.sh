209e6434347eccf463a718649f3a79f88c598196
Benjamin Weidig <ben@netzgut.net>
2018-04-07 10:47:51 +0200

    Updated versions in example script

--- setup-example.sh
+++ setup-example.sh
@@ -7,13 +7,13 @@ set -e
 export MARKER_DIRECTORY="$HOME/.idempotent"
 
 # SETTINGS FOR SCRIPTS
-export DOCKER_COMPOSE_VERSION="1.17.1"
+export DOCKER_COMPOSE_VERSION="1.20.1"
 export GIT_FLOW_REPO_HOME="$HOME/.gitflow"
-export JETBRAINS_TOOLBOX_VERSION="1.6.2914"
+export JETBRAINS_TOOLBOX_VERSION="1.8.3678"
 export JIRACLIENT_VERSION="3_8_4"
-export NVM_VERSION="0.33.6"
-export ANDROID_STUDIO_URL=https://dl.google.com/dl/android/studio/ide-zips/3.0.1.0/android-studio-ide-171.4443003-linux.zip
-export GOLANG_VERSION="1.9.2"
+export NVM_VERSION="0.33.8"
+export ANDROID_STUDIO_URL=https://dl.google.com/dl/android/studio/ide-zips/3.1.0.16/android-studio-ide-173.4670197-linux.zip
+export GOLANG_VERSION="1.10.1"
 
 
 # DO NOT REMOVE THIS IS REQUIRED
