84abf60639ffa9420c4f882cbfbebfd44b049cf1
Benjamin Weidig <ben@netzgut.net>
2018-04-07 10:47:12 +0200

    Added Java 9

---
+++ scripts/java9.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+NAME="Java 9 (Oracle)"
+MARKER="oracle-java9"
+
+echo "Trying to install $NAME"
+
+if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
+    sudo add-apt-repository -y ppa:webupd8team/java \
+    && echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections \
+    && sudo apt-get update \
+    && sudo apt-get install -y --no-install-recommends \
+        oracle-java9-installer \
+        oracle-java9-set-default \
+    && date > $MARKER_DIRECTORY/$MARKER \
+    && echo "Finished install $NAME"
+else
+    echo "$NAME is already installed"
+fi
