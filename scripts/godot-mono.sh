d6e3662eabacbc3f3c87f0af772a9ba94db7bc21
Benjamin Weidig <ben@netzgut.net>
2018-04-15 11:49:28 +0200

    Added godot-mono

---
+++ scripts/godot-mono.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+###############################################################################
+# Godot 3.0 Game engine with Mono Support
+###############################################################################
+# URL: https://godotengine.org/
+###############################################################################
+
+NAME="Godot with Mono-Support"
+MARKER="godot-mono"
+
+###############################################################################
+
+# GODOT_VERSION
+[ -n "${GODOT_VERSION}" ] && GODOT_VERSION=3.0.2
+
+###############################################################################
+
+GODOT_PATH="/opt/godot-${GODOT_VERSION}"
+GODOT_NAME="Godot_v${GODOT_VERSION}-stable_mono_x11_64"
+GODOT_MONO_URL=https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/mono/${GODOT_NAME}.zip
+
+echo "Trying to install $NAME"
+
+if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
+    curl -L ${GODOT_MONO_URL} -o /tmp/${GODOT_NAME}.zip \
+    && sudo unzip /tmp/${GODOT_NAME}.zip -C $GODOT_PATH \
+    && mkdir -p $HOME/bin \
+    && ln -s ${GODOT_PATH}/${GODOT_NAME} $BIN_DIRECTORY/godot \
+    && date > $MARKER_DIRECTORY/$MARKER \
+    && echo "Finished installing $NAME"
+else
+    echo "$NAME is already installed"
+fi
