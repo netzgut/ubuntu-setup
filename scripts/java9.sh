8e0d5ddf4a6db1cb4f83054f1d383a302de6656e
Benjamin Weidig <ben@netzgut.net>
2018-04-15 11:52:21 +0200

    Java9 typo fix

--- scripts/java9.sh
+++ scripts/java9.sh
@@ -9,7 +9,7 @@ echo "Trying to install $NAME"
 
 if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
     sudo add-apt-repository -y ppa:webupd8team/java \
-    && echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections \
+    && echo oracle-java9-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections \
     && sudo apt-get update \
     && sudo apt-get install -y --no-install-recommends \
         oracle-java9-installer \
