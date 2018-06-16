e4832554871184188f9311af866b7406d67199bb
Benjamin Weidig <ben@netzgut.net>
2018-06-16 19:04:40 +0200

    docker: docker-compose 1.21.2

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -14,7 +14,7 @@ MARKER="docker"
 ###############################################################################
 
 # DOCKER_COMPOSE_VERSION
-[ -z "${DOCKER_COMPOSE_VERSION}" ] && DOCKER_COMPOSE_VERSION="1.21.0"
+[ -z "${DOCKER_COMPOSE_VERSION}" ] && DOCKER_COMPOSE_VERSION="1.21.2"
 
 ###############################################################################
 
