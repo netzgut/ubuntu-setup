eeedc0eda840048c2c892d9ebd1e2863daa79017
Benjamin Weidig <ben@netzgut.net>
2018-07-01 18:45:59 +0200

    Added watchman

---
+++ scripts/watchman.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+###############################################################################
+# Facebook Watchman
+###############################################################################
+# URL: https://facebook.github.io/watchman/
+###############################################################################
+
+NAME="Facebook Watchman"
+MARKER="watchman"
+
+###############################################################################
+
+# WATCHMAN_VERSION
+[ -z "${WATCHMAN_VERSION}" ] && WATCHMAN_VERSION="v4.9.0"
+
+# INOTIFY_MAX_USER_WATCHES
+[ -z "${INOTIFY_MAX_USER_WATCHES}" ] && INOTIFY_MAX_USER_WATCHES="999999"
+# INOTIFY_MAX_QUEUED_EVENTS
+[ -z "${INOTIFY_MAX_QUEUED_EVENTS}" ] && INOTIFY_MAX_QUEUED_EVENTS="999999"
+# INOTIFY_MAX_USER_INSTANCES
+[ -z "${INOTIFY_MAX_USER_INSTANCES}" ] && INOTIFY_MAX_USER_INSTANCES="999999"
+
+###############################################################################
+
+echo "Trying to install $NAME"
+
+if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
+    sudo apt-get install -y autoconf automake build-essential python-dev libssl-dev libtool \
+    && git clone https://github.com/facebook/watchman.git /tmp/watchman \
+    && cd /tmp/watchman \
+    && git checkout $WATCHMAN_VERSION \
+    && ./autogen.sh \
+    && ./configure \
+    && make \
+    && sudo make install \
+    && echo $INOTIFY_MAX_USER_WATCHES | sudo tee -a /proc/sys/fs/inotify/max_user_watches \
+    && echo $INOTIFY_MAX_QUEUED_EVENTS | sudo tee -a  /proc/sys/fs/inotify/max_queued_events \
+    && echo $INOTIFY_MAX_USER_INSTANCES | sudo tee  -a /proc/sys/fs/inotify/max_user_instances \
+    && watchman  shutdown-server \
+    && sudo rm -rf /tmp/watchman \
+    && date > $MARKER_DIRECTORY/$MARKER \
+    && echo "Finished installing $NAME"
+else
+    echo "$NAME is already installed"
+fi
