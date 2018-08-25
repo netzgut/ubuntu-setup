b337f7946fdfc05838c150f4edfad782999bdb84
Benjamin Weidig <ben@netzgut.net>
2018-08-25 11:20:09 +0200

    vscode-extensions: fixed no longer available extensions

--- scripts/vscode-extensions.sh
+++ scripts/vscode-extensions.sh
@@ -19,7 +19,6 @@ if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
     code --install-extension 2gua.rainbow-brackets \
     && code --install-extension PeterJausovec.vscode-docker \
     && code --install-extension Zignd.html-css-class-completion \
-    && code --install-extension bschulte.love \
     && code --install-extension dbaeumer.vscode-eslint \
     && code --install-extension donjayamanne.githistory \
     && code --install-extension flowtype.flow-for-vscode \
@@ -27,7 +26,6 @@ if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
     && code --install-extension msjsdiag.debugger-for-chrome \
     && code --install-extension robertohuertasm.vscode-icons \
     && code --install-extension robinbentley.sass-indented \
-    && code --install-extension patrys.vscode-code-outline \
     && code --install-extension rust-lang.rust \
     && date > $MARKER_DIRECTORY/$MARKER \
     && echo "Finished installing $NAME"
