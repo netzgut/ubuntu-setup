df5fa1724e5c075f6392398ddd7d8121434b1cdc
Benjamin Weidig <ben@netzgut.net>
2019-03-11 18:48:42 +0900

    jetbrains-toolbox: updated to 1.13.4801

--- scripts/jetbrains-toolbox.sh
+++ scripts/jetbrains-toolbox.sh
@@ -14,7 +14,7 @@ MARKER="jetbrains-toolbox"
 ###############################################################################
 
 # JETBRAINS_TOOLBOX_VERSION
-[ -z "${JETBRAINS_TOOLBOX_VERSION}" ] && JETBRAINS_TOOLBOX_VERSION="1.11.4231"
+[ -z "${JETBRAINS_TOOLBOX_VERSION}" ] && JETBRAINS_TOOLBOX_VERSION="1.13.4801"
 
 ###############################################################################
 
