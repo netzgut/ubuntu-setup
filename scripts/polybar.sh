d8793e1097d47c031a2027206277b327d21d230e
Benjamin Weidig <ben@netzgut.net>
2019-03-18 17:28:51 +0900

    polybar: added

---
+++ scripts/polybar.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+###############################################################################
+# Polybar
+###############################################################################
+# URL: https://github.com/jaagr/polybar
+###############################################################################
+
+NAME="Polybar"
+MARKER="polybar"
+
+###############################################################################
+
+echo "Trying to install $NAME"
+
+if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
+    sudo apt-get install -y --no-install-recommends \
+        cmake \
+        cmake-data \
+        libcairo2-dev \
+        libxcb1-dev \
+        libxcb-ewmh-dev \
+        libxcb-icccm4-dev \
+        libxcb-image0-dev \
+        libxcb-randr0-dev \
+        libxcb-util0-dev \
+        libxcb-xkb-dev \
+        pkg-config \
+        python-xcbgen \
+        xcb-proto \
+        libxcb-xrm-dev \
+        i3-wm \
+        libasound2-dev \
+        libmpdclient-dev \
+        libiw-dev \
+        libcurl4-openssl-dev \
+        libpulse-dev \
+    && git clone https://github.com/jaagr/polybar.git /tmp/polybar \
+    && cd /tmp/polybar \
+    && git checkout "3.3.0" \
+    && ./build.sh \
+s    && date > $MARKER_DIRECTORY/$MARKER \
+    && echo "Finished installing $NAME"
+else
+    echo "$NAME is already installed"
+fi
