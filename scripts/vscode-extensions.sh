c66a54da82d462cfa7fd4baf9fce450a5e3cdd77
Benjamin Weidig <ben@netzgut.net>
2019-04-07 14:27:38 +0200

    vscode-extensions: updatet to my current setup

--- scripts/vscode-extensions.sh
+++ scripts/vscode-extensions.sh
@@ -18,17 +18,20 @@ echo "Trying to install $NAME"
 if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
     code --install-extension 2gua.rainbow-brackets \
     && code --install-extension alefragnani.Bookmarks \
-    && code --install-extension Zignd.html-css-class-completion \
-    && code --install-extension esbenp.prettier-vscode \
-    && code --install-extension donjayamanne.githistory \
-    && code --install-extension eamodio.gitlens \
-    && code --install-extension flowtype.flow-for-vscode \
     && code --install-extension dbaeumer.vscode-eslint \
+    && code --install-extension donjayamanne.githistory \
+    && code --install-extension dracula-theme.theme-dracula
+    && code --install-extension eamodio.gitlens \
+    && code --install-extension esbenp.prettier-vscode \
+    && code --install-extension flowtype.flow-for-vscode \
     && code --install-extension ms-vscode.Go \
     && code --install-extension msjsdiag.debugger-for-chrome \
-    && code --install-extension vscode-icons-team.vscode-icons \
+    && code --install-extension patrys.vscode-code-outline \
+    && code --install-extension robinbentley.sass-indented \
     && code --install-extension robinbentley.sass-indented \
     && code --install-extension rust-lang.rust \
+    && code --install-extension vscode-icons-team.vscode-icons \
+    && code --install-extension Zignd.html-css-class-completion \
     && date > $MARKER_DIRECTORY/$MARKER \
     && echo "Finished installing $NAME"
 else
