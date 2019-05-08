6b68f383520e3ffdf58b559fb27496f4daaf7d98
Benjamin Weidig <ben@netzgut.net>
2019-05-08 11:55:54 +0200

    openjdk11: added openjfx

--- scripts/openjdk11.sh
+++ scripts/openjdk11.sh
@@ -22,6 +22,8 @@ if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
         openjdk-11-jdk \
         openjdk-11-doc \
         openjdk-11-source \
+        openjfx \
+        openjfx-source \
     && date > $MARKER_DIRECTORY/$MARKER \
     && echo "Finished install $NAME"
 else
