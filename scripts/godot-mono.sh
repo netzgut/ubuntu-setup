85ffa87ed02a123ff808bc4829b47c2f9588c463
Benjamin Weidig <ben@netzgut.net>
2019-05-21 23:10:06 +0200

    godot-mono: 3.1.1 + requirements added

--- scripts/godot-mono.sh
+++ scripts/godot-mono.sh
@@ -14,23 +14,29 @@ MARKER="godot-mono"
 ###############################################################################
 
 # GODOT_VERSION
-[ -z "${GODOT_VERSION}" ] && GODOT_VERSION=3.1
+[ -z "${GODOT_VERSION}" ] && GODOT_VERSION=3.1.1
 
 ###############################################################################
 
-GODOT_PATH="/opt/godot-${GODOT_VERSION}"
+GODOT_PATH="/opt/"
 GODOT_NAME="Godot_v${GODOT_VERSION}-stable_mono_x11_64"
+GODOT_EXEC="Godot_v${GODOT_VERSION}-stable_mono_x11.64"
 GODOT_MONO_URL=https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/mono/${GODOT_NAME}.zip
 GODOT_TEMP_FILE="$(mktemp -u).zip"
 
 echo "Trying to install $NAME"
 
 if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
-    curl -L ${GODOT_MONO_URL} -o $GODOT_TEMP_FILE \
-    && sudo unzip $GODOT_TEMP_FILE -C $GODOT_PATH \
+    && sudo apt install gnupg ca-certificates \
+    && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF \
+    && echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list \
+    && sudo apt update \
+    && sudo apt install -y --no-install-recommends mono-devel \
+    && curl -L ${GODOT_MONO_URL} -o $GODOT_TEMP_FILE \
+    && sudo unzip $GODOT_TEMP_FILE -d $GODOT_PATH \
     && rm $GODOT_TEMP_FILE \
     && mkdir -p $HOME/bin \
-    && ln -s ${GODOT_PATH}/${GODOT_NAME} $BIN_DIRECTORY/godot \
+    && ln -s ${GODOT_PATH}/${GODOT_NAME}/${GODOT_EXEC} $BIN_DIRECTORY/godot \
     && date > $MARKER_DIRECTORY/$MARKER \
     && echo "Finished installing $NAME"
 else
