17ab29fe9ab330565a47f89d75f16465f6e8460a
Benjamin Weidig <ben@netzgut.net>
2019-06-07 21:41:25 +0200

    vscode-extensions: fix script, remove dead ext

--- scripts/vscode-extensions.sh
+++ scripts/vscode-extensions.sh
@@ -21,14 +21,12 @@ if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
     && code --install-extension cospaia.clojure4vscode \
     && code --install-extension dbaeumer.vscode-eslint \
     && code --install-extension donjayamanne.githistory \
-    && code --install-extension dracula-theme.theme-dracula
+    && code --install-extension dracula-theme.theme-dracula \
     && code --install-extension eamodio.gitlens \
     && code --install-extension esbenp.prettier-vscode \
     && code --install-extension flowtype.flow-for-vscode \
     && code --install-extension ms-vscode.Go \
     && code --install-extension msjsdiag.debugger-for-chrome \
-    && code --install-extension patrys.vscode-code-outline \
-    && code --install-extension robinbentley.sass-indented \
     && code --install-extension robinbentley.sass-indented \
     && code --install-extension rust-lang.rust \
     && code --install-extension vscode-icons-team.vscode-icons \
