09a64532efcc46101f62776840828df9c5531c0b
Benjamin Weidig <ben@netzgut.net>
2019-07-11 20:05:40 +0200

    golang: update to 1.12.7

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -14,18 +14,18 @@ MARKER="golang"
 ###############################################################################
 
 # GOLANG_VERSION
-[ -z "${GOLANG_VERSION}" ] && GOLANG_VERSION="1.12.5"
+[ -z "${GOLANG_VERSION}" ] && GOLANG_VERSION="1.12.7"
 
 ###############################################################################
 
-GODOT_TEMP_FILE="$(mktemp -u).tar.gz"
+GO_TEMP_FILE="$(mktemp -u).tar.gz"
 
 echo "Trying to install $NAME"
 
 if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
-    curl -L https://storage.googleapis.com/golang/go${GOLANG_VERSION}.linux-amd64.tar.gz -o $GODOT_TEMP_FILE \
+    curl -L https://dl.google.com/go/go${GOLANG_VERSION}.linux-amd64.tar.gz -o $GO_TEMP_FILE \
     && sudo tar -C /usr/local -xzf $GODOT_TEMP_FILE \
-    && rm $GODOT_TEMP_FILE \
+    && rm $GO_TEMP_FILE \
     && date > $MARKER_DIRECTORY/$MARKER \
     && echo "Finished installing $NAME"
 else
