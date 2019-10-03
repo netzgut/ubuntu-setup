0ee7e175aef31652ba4ed993db349d80ac0c9459
Benjamin Weidig <ben@netzgut.net>
2019-10-03 16:19:37 +0200

    added powertop

---
+++ scripts/powertop.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+MARKER=$(basename "${BASH_SOURCE%.*}")
+
+###############################################################################
+# Powertop
+###############################################################################
+# URL: https://wiki.archlinux.org/index.php/Powertop
+###############################################################################
+
+NAME="Powertop"
+
+###############################################################################
+
+print_banner "$NAME"
+
+if [ ! -f $MARKER_DIRECTORY/$MARKER ]; then
+    sudo apt install -y \
+        powertop
+    && echo "[Unit]
+Description=Powertop tunings
+
+[Service]
+Type=exec
+ExecStart=/usr/bin/powertop --auto-tune
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/powertop.service \
+    && finish_install $MARKER
+else
+    already_installed $MARKER
+fi
