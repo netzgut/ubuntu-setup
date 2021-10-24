836ce04829ef3688e890a2ce0057360806273bb1
Ben Weidig <ben@netzgut.net>
2021-10-24 12:06:05 +0200

    i3: update keyring

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -21,7 +21,7 @@ KEY_TEMP_FILE="$(mktemp -u).deb"
 print_banner "$NAME"
 
 if [ ! -f "$MARKER_DIRECTORY"/"$MARKER" ]; then
-    /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2020.02.03_all.deb $KEY_TEMP_FILE SHA256:c5dd35231930e3c8d6a9d9539c846023fe1a08e4b073ef0d2833acd815d80d48 \
+    /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2021.02.02_all.deb "$KEY_TEMP_FILE" SHA256:cccfb1dd7d6b1b6a137bb96ea5b5eef18a0a4a6df1d6c0c37832025d2edaa710 \
     && sudo dpkg -i "$KEY_TEMP_FILE" \
     && rm "$KEY_TEMP_FILE" \
     && echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | sudo cut -f2 -d=) universe" | sudo tee --append /etc/apt/sources.list.d/sur5r-i3.list \
